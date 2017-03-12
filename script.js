ccee7c6fd386e4d1f9b91367e165351bcb98cb3d
Courtney Noonan <courtneynoonan@me.com>
2017-03-12 13:02:25 -0400

    adding icons for weather status

--- script.js
+++ script.js
@@ -11,15 +11,18 @@ $(document).ready(function() {
 
         $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=d1df01200c747f524de64c3144b17224", function(json) {
             weather = json;
-            console.log(weather);
             $("#city").html(weather.name + ", " + weather.sys.country);
             $("#temperature").html(weather.main.temp + " C");
             $("#main").html(weather['weather']['0'].description);
+            var iconId = weather['weather']['0'].id
+            console.log(iconId)
+            $('.icon').addClass('wi wi-owm-' + iconId);
         });
     }, function(error) {
       console.log(error);
     });
   }
+
 
 
 
