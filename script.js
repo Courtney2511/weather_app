f82b49347601609446cff3c22a50d2cb23a80196
Courtney Noonan <courtneynoonan@me.com>
2017-03-12 15:35:13 -0400

    completing app

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 $(document).ready(function() {
 
-  var latitude = "";
-  var longitude = "";
-  var weather
+  var farenheit = "";
+  var celsius = ""
+
 
   if (navigator.geolocation) {
+
     navigator.geolocation.getCurrentPosition(function(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
 
         $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=d1df01200c747f524de64c3144b17224", function(json) {
             weather = json;
+            celsius = Math.floor(weather.main.temp)
             $("#city").html(weather.name + ", " + weather.sys.country);
-            $("#temperature").html(weather.main.temp + " C");
+            $("#unit").text(celsius + ' C');
             $("#main").html(weather['weather']['0'].description);
             var iconId = weather['weather']['0'].id
-            console.log(iconId)
             $('.icon').addClass('wi wi-owm-' + iconId);
+
+            farenheit = (celsius * (9/5)) + 32
+
+        }).done(function(){
+          $('#unit').click(function(){
+            $('#unit').text($(this).text() == celsius + ' C' ? farenheit + ' F' : celsius + ' C');
+          });
         });
-    }, function(error) {
-      console.log(error);
-    });
-  }
-
-
-
-
-
-
-
-
-
+      });
+    }
 });
